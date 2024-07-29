5b347a1fd8e861e5332bc7c9b52f90bed75619a5
octaviosancho-dev <octaviosanchodev@gmail.com>
2024-07-29 17:48:04 -0300

    stripe payments and checkout models implementation

---
+++ Mango.Web/Models/StripeRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using Mango.Web.Models.Dto;
+
+namespace Mango.Web.Models
+{
+    public class StripeRequestDto
+    {
+        public string StripeSessionUrl { get; set; }
+        public string StripeSessionId { get; set; }
+        public string ApprovedUrl { get; set; }
+        public string CancelUrl { get; set; }
+        public OrderHeaderDto OrderHeader { get; set; }
+    }
+}
