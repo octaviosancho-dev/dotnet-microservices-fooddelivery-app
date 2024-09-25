b38fcb4061ca895fc93594612fb3e9ce8bc9f43e
octaviosancho-dev <octaviosanchodev@gmail.com>
2024-09-24 23:27:17 -0300

    database connection changed

--- FoodDelivery.Services.AuthAPI/Data/AppDbContext.cs
+++ FoodDelivery.Services.AuthAPI/Data/AppDbContext.cs
@@ -1,5 +1,4 @@
 ﻿using FoodDelivery.Services.AuthAPI.Models;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
--- FoodDelivery.Services.AuthAPI/Program.cs
+++ FoodDelivery.Services.AuthAPI/Program.cs
@@ -12,7 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 // EF Core and SQL Server Db
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlConnection"));
 });
 // Poppulate JwtOptions Model
 builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection("ApiSettings:JwtOptions"));
